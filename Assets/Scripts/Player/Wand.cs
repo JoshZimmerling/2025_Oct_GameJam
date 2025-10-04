119dfeb02e2e87d660fbb914d69cd79b369e854b
Kaiden Yates <ksetay@20cents.net>
2025-10-04 13:45:04 -0500

    Implement basicAttack framework

---
+++ Assets/Scripts/Player/Wand.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Wand : MonoBehaviour
+{
+    private InputAction basicAttack;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        basicAttack = InputSystem.actions.FindAction("Attack");
+        
+        basicAttack.Enable();
+        basicAttack.performed += OnAttack;
+    }
+
+    private void OnDisable()
+    {
+        basicAttack.performed -= OnAttack;
+        basicAttack.Disable();
+    }
+
+    private void OnAttack(InputAction.CallbackContext context)
+    {
+        Debug.Log("Attack triggered");
+    }
+    
+}
