ef6e6772e8e65b218a949d9c9f489b6037e14d92
Kaiden Yates <ksetay@20cents.net>
2025-10-18 23:01:18 -0500

    birb healthbar and fireballs die

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -10,10 +10,13 @@ public class Enemy : MonoBehaviour
     public Color damageColor = Color.red;
     public float damageLength = 0.1f;
 
-    public int health = 10;
+    public int startingHealth = 10;
+    public int currentHealth = 10;
     public bool canMove = true;
 
-    public Rigidbody2D rb; 
+    public Rigidbody2D rb;
+
+    public GameObject healthbar;
 
     void Start()
     {
@@ -25,9 +28,11 @@ public class Enemy : MonoBehaviour
     public void Damage(int damage)
     {
         StartCoroutine(Flash());
-        health -= damage;
+        currentHealth -= damage;
 
-        if (health <= 0)
+        healthbar.transform.localScale = new Vector3((float)currentHealth / (float) startingHealth, 1 ,  1);
+        
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
